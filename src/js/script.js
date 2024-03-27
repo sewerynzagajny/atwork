a6302ba42538b105f1f9eca8277c0c51e95e91ac
sewerynzagajny <seweryn.zagajny@gmail.com>
2024-03-28 00:29:20 +0100

    ver3

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,12 @@
-// import 'core-js/stable';
-// import 'regenerator-runtime/runtime';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
+if (module.hot) {
+  module.hot.accept();
+}
 
 // Data
 const dateContainerEL = document.querySelector('.date__container');
-
-// const day = `${now.getDate()}`.padStart(2, 0);
-// const mounth = `${now.getMonth() + 1}`.padStart(2, 0);
-// const year = now.getFullYear();
-// const hour = `${now.getHours()}`.padStart(2, 0);
-// const min = `${now.getMinutes()}`.padStart(2, 0);
-// const sec = `${now.getSeconds()}`.padStart(2, 0);
 
 const updateDate = function () {
   const now = new Date();
@@ -34,6 +27,3 @@ const updateDate = function () {
 };
 setInterval(updateDate, 1000);
 updateDate();
-
-// dateContainerEL.textContent = `${day}.${mounth}.${year}, ${hour}:${min}:${sec}`;
-// console.log(`${day}.${mounth}.${year}, ${hour}:${min}:${sec}`);
